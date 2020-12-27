fdc8b1c9f7cf8ae43b04764749812753f1dd5716
timtim <38246876+hajdaini@users.noreply.github.com>
2020-12-27 16:00:14 +0100

    Update install.sh

--- zshrc/install.sh
+++ zshrc/install.sh
@@ -10,7 +10,6 @@ install_zsh(){
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     printf("vim ~/.p10k.zs \n to enabla CPU/ram/disk_usage")
