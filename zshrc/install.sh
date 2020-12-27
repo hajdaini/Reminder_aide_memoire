064eccbb0ce1c224847d63465d39fd21cdb786b0
timtim <38246876+hajdaini@users.noreply.github.com>
2020-12-27 15:59:44 +0100

    Create install.sh

---
+++ zshrc/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo  apt-get update -y
+
+install_zsh(){
+    apt-get install -y zsh
+    chsh --shell /bin/zsh
+    /bin/zsh
+    printf("Logout/Login to see your zsh as your default interperter")
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    printf("vim ~/.p10k.zs \n to enabla CPU/ram/disk_usage")
+}
+
+install_zsh
